a65564912f5fffe70779fe1ede669820c817cff1
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-11-19 16:41:23 +0300

    feat: complite 'Returns an angle (in radians) between two vectors' lesson

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -113,8 +113,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const numerator = x1 * x2 + y1 * y2;
+  const denominator =
+    Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2);
+  const angleBetweenVectors = Math.acos(numerator / denominator);
+
+  return angleBetweenVectors;
 }
 
 /**
