260cf82e039cc780e670a9255817b574c4b1d7b5
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-11-20 18:51:57 +0300

    feat: complite 'Tries to convert value to number and returns it if conversion was successful' lesson

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -239,8 +239,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const valueToNumber = Number.isNaN(Number(value)) ? def : Number(value);
+  return valueToNumber;
 }
 
 /**
